36331e5f3a8078488cb95abcff42f4d797c17f64
Shkelqim Behluli <shkelqimbehluli@gmail.com>
2017-02-05 15:14:04 +0100

    Template and CSS files are aded.

--- app/mod_main/views.py
+++ app/mod_main/views.py
@@ -1,7 +1,16 @@
-from flask import Blueprint
+from flask import Blueprint,render_template
 
 mod_main = Blueprint('main', __name__)
 
 @mod_main.route('/')
 def index():
-	return "Flask Hello World"
+	return render_template("index.html")
+
+@mod_main.route('/form')
+def form():
+	name = "TechStitution Py."
+	return render_template("form.html",param1 = name )
+
+@mod_main.route('/about')
+def about():
+	return render_template("about.html")
